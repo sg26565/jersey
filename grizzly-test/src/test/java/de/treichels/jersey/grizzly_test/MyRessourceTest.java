9a38ee0f1aef950b92a2ef4facc0ad20599569dc
Oliver Treichel <oli@.treichels.de>
2015-11-06 12:04:07 +0100

    consolidated test classes

--- grizzly-test/src/test/java/de/treichels/jersey/grizzly_test/MyRessourceTest.java
+++ grizzly-test/src/test/java/de/treichels/jersey/grizzly_test/MyRessourceTest.java
@@ -9,13 +9,13 @@ import org.glassfish.jersey.test.JerseyTest;
 import org.glassfish.jersey.test.TestProperties;
 import org.junit.Test;
 
-public class MyRessourceFrameworkTest extends JerseyTest {
+public class MyRessourceTest extends JerseyTest {
 	@Override
 	protected Application configure() {
 		enable(TestProperties.LOG_TRAFFIC);
 		enable(TestProperties.DUMP_ENTITY);
 		
-		return new ResourceConfig().packages("de.treichels.jersey.grizzly_test");
+		return new ResourceConfig(MyResource.class);
 	}
 	
 	@Test
